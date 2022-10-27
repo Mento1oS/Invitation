fa6a47f4c74bb5d082eb926ce13f2af5922f5cd4
Mento1oS <103687418+Mento1oS@users.noreply.github.com>
2022-10-27 10:35:08 +0300

    Delete all directory

--- all/js.js
+++
@@ -1,9 +0,0 @@
-let button = document.querySelector('.blob-btn');
-let body = document.querySelector('.body');
-let trick = document.querySelector('.trick');
-console.log(button);
-button.addEventListener('click', () => {
-    body.classList.remove('none');
-    trick.style.display = "none";
-    console.log("Ну просил же");
-});
